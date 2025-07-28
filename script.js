0fbd05e0b6c2d7acb2978bd62d8fb639f10df129
unknown <you@example.com>
2025-07-28 19:11:50 +0530

    cv updates

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function sendMessage() {
     const phone = document.getElementById('phone').value;
     const message = document.getElementById('message').value;
 
-    const whatsappNumber = 7736571253; 
+    const whatsappNumber = 9496020230; 
     const text = `Name: ${name}%0AEmail: ${email}%0APhone: ${phone}%0AMessage: ${message}`;
+    console.log(text)
 
     const url = `https://wa.me/${whatsappNumber}?text=${text}`;
     window.open(url, '_blank');
